b7936575ae4d8f4aedbd62a87497e9e32797f61a
Filippo Pedrazzini <filippo.pedrazzini@joandko.io>
2025-07-15 19:01:08 +0200

    feat(migrations): replace StackAuth with Clerk in database schema to align with new user management system refactor(migrations): remove outdated migration files related to StackAuth to clean up the migration history fix(migrations): update snapshot files to reflect the new database schema changes and ensure consistency

--- lib/db/migrations/0002_nifty_fat_cobra.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE "user_subscriptions" ALTER COLUMN "subscription_id" DROP NOT NULL;--> statement-breakpoint
-ALTER TABLE "user_subscriptions" ALTER COLUMN "product_id" DROP NOT NULL;--> statement-breakpoint
-ALTER TABLE "user_subscriptions" ALTER COLUMN "current_period_start" DROP NOT NULL;--> statement-breakpoint
-ALTER TABLE "user_subscriptions" ALTER COLUMN "current_period_end" DROP NOT NULL;
